002c3374e62b2d5e99ab8708ac94155d66e63e3a
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2022-12-23 13:36:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ PORT = os.environ.get("PORT", "8080")
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filestorenk")
-"
+
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001219225979"))
 
